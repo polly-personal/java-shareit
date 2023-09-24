0a757a0e799c5255291745905cb5a15d7d2c1e5c
PollyPersonal <polly.personal.01@yandex.ru>
2023-09-24 18:17:21 +0300

    2-nd iteration: (fix:15)

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShareItApp {
 
     public static void main(String[] args) {
-        System.setProperty("server.port", "8081");
+//        System.setProperty("server.port", "8081");
         SpringApplication.run(ShareItApp.class, args);
     }
 
